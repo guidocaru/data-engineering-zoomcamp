2e71a36ca092696397b9da0013d9c8fedb7c1f0c
gcaru <carugatiguido@gmail.com>
2025-03-04 03:41:45 -0300

    update homework 5

--- homework-5/download_data.sh
+++ homework-5/download_data.sh
@@ -1,2 +1,14 @@
-mkdir -p data/raw
-wget -P data/raw https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-10.parquet
+#!/bin/bash
+
+DATA_DIR="data/raw"
+
+mkdir -p "$DATA_DIR"
+
+URLS=(
+  "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-10.parquet"
+  "https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv"
+)
+
+for URL in "${URLS[@]}"; do
+  wget -P "$DATA_DIR" "$URL"
+done
